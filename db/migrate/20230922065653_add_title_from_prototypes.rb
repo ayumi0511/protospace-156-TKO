9fcdbaae56e42ff31d312042df7c6a63564f589d
ayumi0511 <ayumi.tokoeda@gmail.com>
2023-09-23 14:33:09 +0900

    修正２

--- db/migrate/20230922065653_add_title_from_prototypes.rb
+++
@@ -1,4 +0,0 @@
-class AddTitleFromPrototypes < ActiveRecord::Migration[7.0]
-  def change
-  end
-end
--- db/migrate/20230922073953_add_title_from_prototypes.rb
+++ db/migrate/20230922073953_add_title_from_prototypes.rb
@@ -1,8 +1,5 @@
 class AddTitleFromPrototypes < ActiveRecord::Migration[7.0]
   def change
-    add_column :prototypes, :title, :string, null: false
-    add_column :prototypes, :catch_copy, :text, null: false
-    add_column :prototypes, :concept, :text, null: false
     add_reference :prototypes, :user, null: false, foreign_key: true
   end
 end
