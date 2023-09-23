017bb96153cc2c2416fe0f595e72ca5e1128909d
ootaketetsuyuki <tetsuyukiotake@forthewin.co.jp>
2023-09-23 12:00:14 +0900

    エラー発見

--- db/migrate/20230920121355_create_prototypes.rb
+++ db/migrate/20230920121355_create_prototypes.rb
@@ -1,9 +1,10 @@
 class CreatePrototypes < ActiveRecord::Migration[7.0]
   def change
     create_table :prototypes do |t|
-      t.string :name
-      t.text :catchcopy
-      t.text :concept
+      t.string :title, null: false
+      t.text :catch_copy, null: false
+      t.text :concept, null: false
+
     end
   end
 end
---
+++ db/migrate/20230922065653_add_title_from_prototypes.rb
@@ -0,0 +1,4 @@
+class AddTitleFromPrototypes < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
